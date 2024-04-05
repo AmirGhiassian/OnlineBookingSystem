f2b92637effa17737e9339ea3eb7a965dee420c6
Nuggethead2003 <amirrezaghiassian@icloud.com>
2024-04-05 17:08:02 -0400

    Fixed cust

--- OnlineBookingSystem/Models/Customer.cs
+++ OnlineBookingSystem/Models/Customer.cs
@@ -7,18 +7,6 @@ namespace OnlineBookingSystem.Models
 {
     public class Customer : IdentityUser //Model class for a single customer, used for registration within the CustomerRepo
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        public string NormalizedUserName { get; set; }
-
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-        [MaybeNull]
-
         public List<Reservation> Reservations { get; set; }
     }
 }
