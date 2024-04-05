ea8242ef5ed89db6897d3bdcdab109d9155fa1cc
Nuggethead2003 <amirrezaghiassian@icloud.com>
2024-04-05 17:09:29 -0400

    Migrations change

---
+++ OnlineBookingSystem/Migrations/20240405210208_FixedCustomer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineBookingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
