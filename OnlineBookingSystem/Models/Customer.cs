d347b3f352271399163ad57c21d08d528f6c3e31
Nuggethead2003 <amirrezaghiassian@icloud.com>
2024-04-05 18:09:59 -0400

    removed obj folder and added gitignore

--- OnlineBookingSystem/Models/Customer.cs
+++ OnlineBookingSystem/Models/Customer.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
 
 
 namespace OnlineBookingSystem.Models
