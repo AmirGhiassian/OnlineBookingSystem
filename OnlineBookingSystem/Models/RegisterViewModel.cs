c34f05b8b9d25e508f6b64083680dc139b448159
Nuggethead2003 <amirrezaghiassian@icloud.com>
2024-04-05 18:20:00 -0400

    Updated Register

--- OnlineBookingSystem/Models/RegisterViewModel.cs
+++ OnlineBookingSystem/Models/RegisterViewModel.cs
@@ -7,15 +7,14 @@ namespace OnlineBookingSystem.Models
     {
         [Required]
         [EmailAddress]
-        public string? Email { get; set; }
+        public required string? Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        public string? Password { get; set; }
+        public required string? Password { get; set; }
 
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    
-        public required string ConfirmPassword { get; set; }
+        public required string? ConfirmPassword { get; set; }
     }
 }
