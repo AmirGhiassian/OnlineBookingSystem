f339503a1db02b52712cfa4f695937cd7f5465d4
Nuggethead2003 <amirrezaghiassian@icloud.com>
2024-04-07 15:11:18 -0400

    Uneeded import

--- OnlineBookingSystem/Models/Customer.cs
+++ OnlineBookingSystem/Models/Customer.cs
@@ -1,7 +1,7 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Identity;
-
 
 namespace OnlineBookingSystem.Models
 {
