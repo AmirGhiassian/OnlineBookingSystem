e999385304562f47988cdbeca886379a1f568f4f
EricHanoun <eric@hanoun.com>
2024-04-07 22:32:23 -0400

    Add XML documentation comments to Restaurant.cs for better code documentation

--- OnlineBookingSystem/Models/Restaurant.cs
+++ OnlineBookingSystem/Models/Restaurant.cs
@@ -1,7 +1,19 @@
 using System.ComponentModel.DataAnnotations;
 
+///<Summary>
+///   Author: Eric Hanoun
+///   This class is used to create a restaurant object that will be used to store restaurant information
+///   in the database. It has a primary key of RestaurantId which is used to uniquely identify a
+///   restaurant in the database. The class has a Name, Address, Phone, Description, and Image field that
+///   will be used to store the restaurant's information. This class also has a list of reservations that
+///   are used to store the reservations that the restaurant has.
+///</Summary>
+
 namespace OnlineBookingSystem.Models
 {
+    /// <summary>
+    /// Model class for a single restaurant, used for storing restaurant information within the database of restaurants
+    /// </summary>
     public class Restaurant
     {
         [Key]
