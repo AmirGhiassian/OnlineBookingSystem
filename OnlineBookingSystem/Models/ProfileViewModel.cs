78650bd859bd5b552d93f26cb760df17e94d1ca9
EricHanoun <eric@hanoun.com>
2024-04-07 22:32:52 -0400

    Add profile view model class with XML documentation comments

--- OnlineBookingSystem/Models/ProfileViewModel.cs
+++ OnlineBookingSystem/Models/ProfileViewModel.cs
@@ -1,5 +1,11 @@
 using System;
 
+/// <summary>
+/// Author: Eric Hanoun
+///  This class is used to create a profile view model object that will be used to store profile information.
+///  This class has a Username, Email, and PhotoPath field that will be used to store the user's profile information.
+/// 
+
 namespace OnlineBookingSystem.Models
 {
     public class ProfileViewModel
