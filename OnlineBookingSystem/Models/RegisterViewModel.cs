1567c6055aec33486cf54abeb664778bb0acbb9f
EricHanoun <eric@hanoun.com>
2024-04-07 22:32:48 -0400

    Add XML documentation comments to RegisterViewModel.cs for better code documentation

--- OnlineBookingSystem/Models/RegisterViewModel.cs
+++ OnlineBookingSystem/Models/RegisterViewModel.cs
@@ -1,8 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// Author: Amir Ghiassian
+/// This class is used to create a register view model object that will be used to store registration information.
+///This class has a Username, PhoneNumber, Email, Password, and ConfirmPassword field that 
+///will be used to store the user's registration information.
+/// </summary>
 namespace OnlineBookingSystem.Models
 {
-    //Model class for a single customer, used for registration within the database of customers
+    /// <summary>
+    /// Model class for a single register view model, used for registration within the database of users
+    /// </summary>
     public class RegisterViewModel
     {
         [Required]
@@ -19,6 +27,9 @@ namespace OnlineBookingSystem.Models
         [DataType(DataType.Password)]
         public required string? Password { get; set; }
 
+        /// <summary>
+        /// Required ConfirmPassword field that will be used to store the user's confirmation password.
+        /// </summary>
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public required string? ConfirmPassword { get; set; }
