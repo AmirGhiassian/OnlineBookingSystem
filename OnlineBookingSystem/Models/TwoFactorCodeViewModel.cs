82d6faac7446f959931c12d0f88a039782781944
EricHanoun <eric@hanoun.com>
2024-04-07 22:32:58 -0400

    Add TwoFactorCodeViewModel class with XML documentation comments

--- OnlineBookingSystem/Models/TwoFactorCodeViewModel.cs
+++ OnlineBookingSystem/Models/TwoFactorCodeViewModel.cs
@@ -1,9 +1,21 @@
 using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// Author: Amir Ghiassian
+/// This class is used to create a TwoFactorCodeViewModel object that will be used to store two factor code information.
+/// It has a required code field that will be used to store the user's two factor code.
+/// </summary>
+
 namespace OnlineBookingSystem.Models
 {
+    /// <summary>
+    /// Model class for a single two factor code, used for two factor authentication within the database of two factor codes
+    /// </summary>
     public class TwoFactorCodeViewModel
     {
+        /// <summary>
+        /// Required code field that will be used to store the user's two factor code.
+        /// </summary>
         [Required]
         public required string code { get; set; }
     }
