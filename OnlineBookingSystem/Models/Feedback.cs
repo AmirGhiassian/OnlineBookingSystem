aa950ddbdbca77525917c8691f818b1c475754a9
EricHanoun <eric@hanoun.com>
2024-04-07 22:32:40 -0400

    Add XML documentation comments to Feedback.cs for better code documentation

--- OnlineBookingSystem/Models/Feedback.cs
+++ OnlineBookingSystem/Models/Feedback.cs
@@ -1,7 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// Author: Amir Ghiassian
+/// This class is used to create a feedback object that will be used to store feedback information.
+/// primary key of FeedbackId which is used to uniquely identify a feedback in the database.
+/// </summary>
+
 namespace OnlineBookingSystem.Models
 {
+    /// <summary>
+    /// Model class for a single feedback, used for storing feedback information within the database of feedback
+    /// </summary>
     public class Feedback
     {
         [Key]
