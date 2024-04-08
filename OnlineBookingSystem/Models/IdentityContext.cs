d854d85e87b359d42daa8854348760aa469e9a10
EricHanoun <eric@hanoun.com>
2024-04-07 22:33:01 -0400

    Update IdentityContext.cs with XML documentation comments and constructor summary

--- OnlineBookingSystem/Models/IdentityContext.cs
+++ OnlineBookingSystem/Models/IdentityContext.cs
@@ -4,19 +4,36 @@ using OnlineBookingSystem.Models;
 
 namespace OnlineBookingSystem.Areas.Identity.Data;
 
+/* Author: 
+*/
+
+/// <summary>
+/// Amir Ghiassian
+/// This class is used to create a context for the identity database. This class inherits from IdentityDbContext
+/// which is a class that is used to store user information in the database. This class has a constructor that
+/// takes in a DbContextOptions object and passes it to the base constructor. This class also has an OnModelCreating
+/// method that takes in a ModelBuilder object and passes it to the base OnModelCreating method. This class is used
+/// to create a context for the identity database.
+/// </summary>
+
 public class IdentityContext : IdentityDbContext<Customer>
 {
+    /// <summary>
+    /// Constructor that takes in a DbContextOptions object and passes it to the base constructor.
+    /// </summary>
+    /// <param name="options"></param>
     public IdentityContext(DbContextOptions<IdentityContext> options)
         : base(options)
     {
 
     }
 
+    /// <summary>
+    /// This method is used to create a model for the identity database.
+    /// </summary>
+    /// <param name="builder"></param>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
     }
 }
