a39b6bd05f3f5433d57625e44a78f3ca3c322e0c
EricHanoun <eric@hanoun.com>
2024-04-07 22:32:55 -0400

    Add XML documentation comments to LoginViewModel.cs for better code documentation

--- OnlineBookingSystem/Models/LoginViewModel.cs
+++ OnlineBookingSystem/Models/LoginViewModel.cs
@@ -1,7 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 
+/* 
+*/
+
+/// <summary>
+/// Author: Daniel O'Brien
+/// This class is used to create a login view model object that will be used to store login information.
+/// It has a required UserName and Password field that will be used to store the user's login information.
+/// 
+
 namespace OnlineBookingSystem.Models
 {
+
+    /// <summary>
+    /// Model class for a single login view model, used for login within the database of users
+    /// </summary>
     public class LoginViewModel
     {
         [Required]
@@ -9,8 +22,5 @@ namespace OnlineBookingSystem.Models
         [Required]
         [DataType(DataType.Password)]
         public required string Password { get; set; }
-
-        //[Display(Name = "Remember me?")]
-        //public bool RememberMe { get; set; }
     }
 }
