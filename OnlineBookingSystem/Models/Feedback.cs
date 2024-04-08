3b6ea214238cdbea4e68bf337765f3672ca9603a
EricHanoun <eric@hanoun.com>
2024-04-08 18:45:32 -0400

    Fix required attribute in Feedback.cs model

--- OnlineBookingSystem/Models/Feedback.cs
+++ OnlineBookingSystem/Models/Feedback.cs
@@ -15,16 +15,10 @@ namespace OnlineBookingSystem.Models
     {
         [Key]
         public int FeedbackId { get; set; }
-        [Required]
+        public int ReservationId { get; set; }
         public required string Name { get; set; }
-
-        [Required]
         public required string Email { get; set; }
-
-        [Required]
         public required string Phone { get; set; }
-
-        [Required]
         public required string Message { get; set; }
     }
 }
